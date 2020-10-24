595f0bab5aa95a09931ee10379d077b5ae2b7886
Raghav Kauhsik <kaushikraghav2@gmail.com>
2020-10-24 13:12:45 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -86,6 +86,99 @@ app.post('/meds/delete/:id',(req,res)=>{
     });
 })
 
+//Get method for edit
+/*
+app.post('/meds/edit/:id',(req,res)=>{
+    const client = new Client({
+
+        user: 'postgres',
+        host: 'localhost',
+        database: 'medical',
+        password: 'raghav',
+        port: 5432,
+    });
+    client.connect()
+    .then(()=>{
+        const sql = 'SELECT * FROM meds WHERE mid=$1';
+        const params = [req.body.mid];
+        client.query(sql,params);
+        
+    })
+    .then((results)=>{
+        console.log('Edit Query');
+        console.log('results?',results);
+        //res.render('medsedit',{med:results.rows[0]});
+    });
+
+})
+*/
+
+/*
+app.post('/meds/edit/:id',(req,res)=>{
+    const client = new Client({
+
+        user: 'postgres',
+        host: 'localhost',
+        database: 'medical',
+        password: 'raghav',
+        port: 5432,
+    });
+    client.connect()
+    .then(()=>{
+        const sql = 'UPDATE meds SET mid=$1,name=$2,count=$3,brand=$4 WHERE mid=$5';
+        const params = [req.body.mid,req.body.name,req.body.count,req.body.brand,req.params.id];
+        client.query(sql,params);
+        
+    })
+    .then((results)=>{
+        res.redirect('/meds');
+    });
+})
+*/
+
+//Dashboard
+app.get('/dashboard',(req,res)=>{
+    const client = new Client({
+
+        user: 'postgres',
+        host: 'localhost',
+        database: 'medical',
+        password: 'raghav',
+        port: 5432,
+    });
+    client.connect()
+    .then(()=>{
+        return client.query('SELECT SUM(count) FROM meds; SELECT DISTINCT COUNT(brand) FROM meds');
+    })
+    .then((results)=>{
+        console.log('?results',results[0]);
+        console.log('?results',results[1]);
+        res.render('dashboard',{n1:results[0].rows,n2:results[1].rows});
+    })
+});
+
+/*
+app.get('/dashboard',(req,res)=>{
+    const client = new Client({
+
+        user: 'postgres',
+        host: 'localhost',
+        database: 'medical',
+        password: 'raghav',
+        port: 5432,
+    });
+    client.connect()
+        .then(()=>{
+            return client.query('SELECT * FROM meds');
+        })
+        .then((results)=>{
+            console.log('results',results);
+            res.render('meds',results);
+        });
+});
+*/
+
+
 //Running server on port 5001
 app.listen(5001,()=>{
     console.log('Listening to port 5001');
